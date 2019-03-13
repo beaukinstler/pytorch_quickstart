1d3995cff0205d4c3854a99549eccef3869357dd
Beau Kinstler <git@beaukinstler.info>
2019-03-13 09:34:20 -0400

    refactor: add option to have more granular exmpl functions

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from app import p
 
 def examples():
     p("Chapter 1 examples",10)
-    wip.examples()
+    wip.basic_examples()
+    wip.operation_examples()
 
 if __name__ == "__main__":
     examples()
--- what_is_pytorch/examples.py
+++ what_is_pytorch/examples.py
@@ -1,6 +1,6 @@
 from what_is_pytorch import *
 
-def examples():
+def basic_examples():
     # p("EXAMPLES")
     # p("Construct a 5x3 matrix, uninitialized:")
     # x = torch.empty(5, 3)
@@ -15,9 +15,9 @@ def examples():
     # print(x)
 
 
-    p("Construct a tensor directly from data:")
-    x = torch.tensor([5.5, 3])
-    print(x)
+    # p("Construct a tensor directly from data:")
+    # x = torch.tensor([5.5, 3])
+    # print(x)
 
     p("""or create a tensor based on an existing tensor. 
          These methods will reuse properties of the input 
@@ -26,6 +26,19 @@ def examples():
     print(x)
     x = torch.randn_like(x, dtype=torch.float)    # override dtype!
     print(x)                                      # result has the same size
+
+    p("Get its size:")
+    print(x.size())  # returns a tuple
+    p("Size object that's returned is a tuple")
+    tup = x.size()
+    print(tup)
+    print(tup[-1])
+    print(tup.count(5))
+
+def operation_examples():
+    p("OPERATIONS")
+    
+
 
 if __name__ == '__main__':
     examples()
